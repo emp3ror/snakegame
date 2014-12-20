2c9c19292f60695c84a0f71202577eda66d5fbdb
emp3ror <thapa.manish16@gmail.com>
2014-12-21 00:20:00 +0545

    snake movement success

--- js/snakegame.js
+++ js/snakegame.js
@@ -1,3 +1,89 @@
 
 var canvas = document.getElementById("snakeboard");
 var ctx = canvas.getContext("2d");
+
+var snake = {
+	
+
+}
+
+var snake = function () {
+	this.sizeSegment = 10;
+	this.startSegments = 4;
+	this.head = "right";
+	this.oppositeHead = {
+		right : "left",
+		left : "right",
+		up : "down",
+		down : "up",
+	};
+	this.width = 500;
+	this.height = 500;
+	this.sizeOfCanvas = 500;
+	this.totalLinearSegments = this.sizeOfCanvas/this.sizeSegment;
+	this.positions = [[5,5],[4,5],[3,5],[2,5]];
+	this.move = function (direction) {
+		var storePosition = this.positions.slice(0);
+		// console.log(storePosition);
+		if (direction==this.oppositeHead[this.head]) {
+
+		} else {
+			if (direction==="right") {
+				this.positions[0] = [this.positions[0][0]+1,this.positions[0][1]];	
+			} else if (direction==="left") {
+				this.positions[0] = [this.positions[0][0]-1,this.positions[0][1]];	
+			} else if (direction==="up") {
+				this.positions[0] = [this.positions[0][0],this.positions[0][1]-1];	
+			} else if (direction==="down") {
+				this.positions[0] = [this.positions[0][0],this.positions[0][1]+1];
+			} 
+			
+			for (var i = 1; i < this.positions.length; i++) {
+				this.positions[i] = storePosition[i-1];
+			};
+			ctx.clearRect(0,0,this.width,this.height);
+			ctx.width= this.width;
+			ctx.height = this.height;
+			this.createSnake();
+			this.head = direction;
+		}
+	};
+	this.createSnake = function () {
+		for (var i = 0; i < this.positions.length; i++) {
+			// this.positions[i]
+			// console.log(this.positions);
+			ctx.save();
+				ctx.fillStyle = "#009900";
+				x = this.positions[i][0]*this.sizeSegment;
+				y = this.positions[i][1]*this.sizeSegment;
+				ctx.fillRect(x,y, this.sizeSegment,this.sizeSegment);
+				ctx.strokeStyle = "#fff";
+				ctx.lineWidth   = 1;
+				ctx.strokeRect(x,y, this.sizeSegment,this.sizeSegment);
+			ctx.restore();
+		};
+	}
+}
+
+var snake1 = new snake();
+snake1.createSnake();
+
+
+/*
+* keyCode = 37 // left arrow
+* keyCode = 38 // up arrow
+* keyCode = 39 // right arrow
+* keyCode = 40 // down arrow
+*/
+
+$(document).keypress(function (event) {
+	if (event.keyCode===37) {
+		snake1.move("left");
+	} else if (event.keyCode===38) {
+		snake1.move("up");
+	} else if (event.keyCode===39) {
+		snake1.move("right");
+	} else if (event.keyCode===40) {
+		snake1.move("down");
+	}
+})
